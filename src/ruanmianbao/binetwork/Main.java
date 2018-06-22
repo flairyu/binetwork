a14f93345f4c7ae35ea010391ac4041be2e36062
Xiangbo Yu <yuxiangbo@umnet.cn>
2018-06-22 17:51:44 +0800

    修改binetwork的格式。

--- src/ruanmianbao/binetwork/Main.java
+++ src/ruanmianbao/binetwork/Main.java
@@ -9,6 +9,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,19 +19,177 @@ public class Main{
 	boolean running = false;
 	Socket socket;
 	ByteOrder order;
+	String script = "";
 	
-	public Main(String server, int port, ByteOrder order) throws UnknownHostException, IOException {
-		outln("connect to:"+server+":"+port);
+	public Main(String[] args) throws Exception {
+		String server = "localhost";
+		int port = 8788;
+		ByteOrder order = ByteOrder.LITTLE_ENDIAN;
+		
+		
+		for(String a: args) {
+			String[] a2 = a.split("=",2);
+			if (a2.length==2) {
+				switch(a2[0]) {
+				case "-s":
+				case "--server":
+					server = a2[1];
+					break;
+				case "-p":
+				case "--port":
+					port = Integer.parseInt(a2[1]);
+					break;
+				case "-e":
+				case "--endian":
+					order = a2[1].equalsIgnoreCase("be")?ByteOrder.BIG_ENDIAN:ByteOrder.LITTLE_ENDIAN;
+					break;
+				case "-r":
+				case "--run":
+					script = a2[1];
+					break;
+				}
+			}
+		}
+		
+		Main.log("connect to:"+server+":"+port);
 		socket = new Socket(server, port);
-		outln("connected.");
+		socket.setSoTimeout(3000);
+		Main.log("connected.");
 		this.order = order;
-		this.running = true;
-		ExecutorService taskExecutor = Executors.newFixedThreadPool(2);
-		try {
-			taskExecutor.execute(new RecvWorker());
-			taskExecutor.execute(new SendWorker());
-		} finally {
-			taskExecutor.shutdown();
+		
+		if (this.script.isEmpty()) {
+			// read from input
+			this.running = true;
+			ExecutorService taskExecutor = Executors.newFixedThreadPool(2);
+			try {
+				taskExecutor.execute(new RecvWorker());
+				taskExecutor.execute(new SendWorker());
+			} finally {
+				taskExecutor.shutdown();
+			}
+		} else {
+			this.running = false;
+			this.RunWorker();
+		}
+		
+	}
+
+	/**
+	 * 进行一次测试
+	 * @throws Exception 
+	 */
+	private void RunWorker() throws Exception {
+		sendData(script);
+		recvData();
+	}
+	
+	private String recvData() throws IOException {
+		int buffer_size = 4096;
+		byte[] buffer = new byte[buffer_size];
+		BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+		int readed = 0, pos = 0;
+		do {
+			readed = in.read(buffer, pos, buffer.length-pos);
+			if (readed>0) pos+=readed;
+		} while(readed>0 && pos<buffer_size);
+		String msg = toHex(buffer,0,pos);
+		Main.log("recv["+pos+"]:"+msg);
+		return msg;
+	}
+	
+	private void sendData(String line) throws IOException {
+		ByteBuffer outbuffer = ByteBuffer.allocate(4*1024);
+		outbuffer.order(order);
+		String[] items = line.split("\\|");
+		for(int i=0; i<items.length; i++) {
+			String[] sp = items[i].split(":");
+			if(sp.length!=3) {
+				Main.log("item ["+i+"] format error:"+items[i]);
+				continue;
+			}
+			switch(sp[0]) {
+			case "b": //二进制
+				putBin(outbuffer, Integer.parseInt(sp[1]), sp[2]);
+				break;
+			case "o": //八进制
+				putOct(outbuffer, Integer.parseInt(sp[1]), sp[2]);
+				break;
+			case "d": //十进制
+				putDec(outbuffer, Integer.parseInt(sp[1]), sp[2]);
+				break;
+			case "x": //十六进制
+				putHex(outbuffer, Integer.parseInt(sp[1]), sp[2]);
+				break;
+			case "s": //字符串
+				putStr(outbuffer, sp[1], sp[2]);
+				break;
+			}
+		}
+		outbuffer.flip();
+		socket.getOutputStream().write(outbuffer.array(), 0, outbuffer.limit());
+		socket.getOutputStream().flush();
+		Main.log("send["+outbuffer.limit()+"]"+toHex(outbuffer.array(), 0, outbuffer.limit()));
+	}
+	
+	// 二进制
+	private void putBin(ByteBuffer outbuffer, int parseInt, String string) {
+		putByte(outbuffer, parseInt, string, 2);
+	}
+	
+	// 八进制
+	private void putOct(ByteBuffer outbuffer, int parseInt, String string) {
+		putByte(outbuffer, parseInt, string, 8);
+	}
+	
+	// 十进制
+	private void putDec(ByteBuffer outbuffer, int parseInt, String string) {
+		putByte(outbuffer, parseInt, string, 10);
+	}
+	
+	// 十六进制
+	private void putHex(ByteBuffer outbuffer, int parseInt, String string) {
+		putByte(outbuffer, parseInt, string, 16);
+	}
+	
+	// 字符串
+	private void putStr(ByteBuffer outbuffer, String encode, String string) throws UnsupportedEncodingException {
+		byte[] data = string.getBytes(encode);
+		outbuffer.put(data);
+	}
+	
+	/**
+	 * 把一个字符串按指定进制转换成字节，存入指定的缓冲区内。如果指定的长度与字符串拆分后的长度不符，则自动补0。
+	 * @param outbuffer 填充的buffer
+	 * @param parseInt 单位：1-byte 2-short 4-int 8-long
+	 * @param string 待处理字符串
+	 * @param radix 进制（2,8,10,16）
+	 */
+	private void putByte(ByteBuffer outbuffer, int parseInt, String string, int radix) {
+		String[] strs = string.split(" ");
+		for(int i=0; i<strs.length; i++) {
+			if (i<strs.length) {
+				switch(parseInt) {
+				case 1:
+					short b = Short.parseShort(strs[i], radix);
+					outbuffer.put((byte)(b&0x00ff));
+					break;
+				case 2:
+					int s = Integer.parseInt(strs[i], radix);
+					outbuffer.putShort((short)(s&0x00ffff));
+					break;
+				case 4:
+					long ii = Long.parseLong(strs[i], radix);
+					outbuffer.putInt((int)(ii&0x00ffffffff));
+					break;
+				case 8:
+					long l = Long.parseLong(strs[i], radix);
+					outbuffer.putLong(l);
+					break;
+				default:
+					Main.log("unsupport type:"+parseInt);
+				}
+				
+			}
 		}
 	}
 
@@ -43,106 +202,14 @@ public class Main{
 			try {
 				Scanner scan = new Scanner(System.in);
 				while (running) {
-					ByteBuffer outbuffer = ByteBuffer.allocate(4*1024);
-					outbuffer.order(order);
 					String line = scan.nextLine();
-					outln("input:"+line);
-					String[] items = line.split("\\|");
-					for(int i=0; i<items.length; i++) {
-						String[] sp = items[i].split(":");
-						if(sp.length!=3) {
-							outln("item ["+i+"] format error:"+items[i]);
-							continue;
-						}
-						switch(sp[0]) {
-						case "b": //二进制
-							putBin(outbuffer, Integer.parseInt(sp[1]), sp[2]);
-							break;
-						case "o": //八进制
-							putOct(outbuffer, Integer.parseInt(sp[1]), sp[2]);
-							break;
-						case "d": //十进制
-							putDec(outbuffer, Integer.parseInt(sp[1]), sp[2]);
-							break;
-						case "x": //十六进制
-							putHex(outbuffer, Integer.parseInt(sp[1]), sp[2]);
-							break;
-						case "s": //字符串
-							putStr(outbuffer, sp[1], sp[2]);
-							break;
-						}
-					}
-					outbuffer.flip();
-					socket.getOutputStream().write(outbuffer.array(), 0, outbuffer.limit());
-					socket.getOutputStream().flush();
-					outln("send["+outbuffer.limit()+"]"+toHex(outbuffer.array(), 0, outbuffer.limit()));
+					Main.log("input:"+line);
+					sendData(line);
 				}
 				scan.close();
 			} catch (Exception e) {
 				running = false;
 				e.printStackTrace();
-			}
-		}
-
-		// 二进制
-		private void putBin(ByteBuffer outbuffer, int parseInt, String string) {
-			putByte(outbuffer, parseInt, string, 2);
-		}
-		
-		// 八进制
-		private void putOct(ByteBuffer outbuffer, int parseInt, String string) {
-			putByte(outbuffer, parseInt, string, 8);
-		}
-		
-		// 十进制
-		private void putDec(ByteBuffer outbuffer, int parseInt, String string) {
-			putByte(outbuffer, parseInt, string, 10);
-		}
-		
-		// 十六进制
-		private void putHex(ByteBuffer outbuffer, int parseInt, String string) {
-			putByte(outbuffer, parseInt, string, 16);
-		}
-		
-		// 字符串
-		private void putStr(ByteBuffer outbuffer, String encode, String string) throws UnsupportedEncodingException {
-			byte[] data = string.getBytes(encode);
-			outbuffer.put(data);
-		}
-		
-		/**
-		 * 把一个字符串按指定进制转换成字节，存入指定的缓冲区内。如果指定的长度与字符串拆分后的长度不符，则自动补0。
-		 * @param outbuffer 填充的buffer
-		 * @param parseInt 单位：1-byte 2-short 4-int 8-long
-		 * @param string 待处理字符串
-		 * @param radix 进制（2,8,10,16）
-		 */
-		private void putByte(ByteBuffer outbuffer, int parseInt, String string, int radix) {
-			String[] strs = string.split(" ");
-			for(int i=0; i<strs.length; i++) {
-				if (i<strs.length) {
-					switch(parseInt) {
-					case 1:
-						short b = Short.parseShort(strs[i], radix);
-						outbuffer.put((byte)(b&0x00ff));
-						break;
-					case 2:
-						int s = Integer.parseInt(strs[i], radix);
-						outbuffer.putShort((short)(s&0x00ffff));
-						break;
-					case 4:
-						long ii = Long.parseLong(strs[i], radix);
-						outbuffer.putInt((int)(ii&0x00ffffffff));
-						break;
-					case 8:
-						long l = Long.parseLong(strs[i], radix);
-						outbuffer.putLong(l);
-						break;
-					default:
-						outln("unsupport type:"+parseInt);
-					}
-					
-				}
 			}
 		}
 	}
@@ -158,7 +225,7 @@ public class Main{
 				BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
 				while (running) {
 					int readed = in.read(buffer, 0, buffer.length);
-					outln("recv["+readed+"]:"+toHex(buffer, 0, readed));
+					Main.log("recv["+readed+"]:"+toHex(buffer, 0, readed));
 				}
 			} catch (Exception e) {
 				running = false;
@@ -171,10 +238,10 @@ public class Main{
 	 * 统一输出
 	 * @param string 要输出的信息
 	 */
-	private void outln(String string) {
-		Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("[HH:mm:ss]");
-		System.out.println(sdf.format(cal.getTime()) + string);
+	static void log(String msg) {
+		Date dNow = new Date( );
+	    SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MM.dd HH:mm:ss");
+		System.out.println("["+ft.format(dNow)+"] "+msg);
 	}
 
 	/**
@@ -182,46 +249,35 @@ public class Main{
 	 * @param args host ip [le|be]
 	 */
 	public static void main(String[] args) {
-		String server = "localhost";
-		int port = 8080;
-		ByteOrder order = ByteOrder.LITTLE_ENDIAN;
-		
-		if (args.length>=1) {
-			server = args[0];
-			if (server.equals("help") || server.equals("-h") || server.equals("?") || server.equals("--help")) {
-				printHelp("java ruanmianbao.binetwork.Main ");
-				System.exit(0);
-			}
+	
+		if (args.length==0 
+				|| args[0].equalsIgnoreCase("help")
+				|| args[0].equalsIgnoreCase("-h")
+				|| args[0].equalsIgnoreCase("?")
+				|| args[0].equalsIgnoreCase("--help")
+				) {
+			printHelp("binetwork");
+			System.exit(0);
 		}
-		
-		if (args.length>=2) {
-			port = Integer.parseInt(args[1]);
-		}
-		
-		if (args.length>=3) {
-			order = args[2]=="be"?
-					ByteOrder.BIG_ENDIAN
-					:ByteOrder.LITTLE_ENDIAN;
-		}
-		Main main = null;
+
 		try {
-			main = new Main(server, port, order);
+			new Main(args);
 		} catch (Exception e) {
-			if (main!=null) main.running = false;
 			e.printStackTrace();
+			System.exit(-1);
 		}
 		
 	}
 
 	private static void printHelp(String cmd) {
 		System.out.println("usage: check 'readme.md' for more info.\n\n" +
-				cmd + "[host or ip] [port] [le|be]\n" +
+				cmd + "[-s|--server]=<host> [-p|--port]=<port> [-e|-endian]=<le|be>\n" +
 				"   -host or ip: the host name or ip (default: localhost)\n"+
 				"   -port: the port of server (default: 8080) \n" +
 				"   -le: little endian (default)\n"+
 				"   -be: big endian \n\n" +
 				"example:\n"+
-				"    "+ cmd + "127.0.0.1 8080 le\n");
+				"    "+ cmd + "-s=127.0.0.1 -p=8080 -e=le -r=\"x:1:fa fb fc fd\"\n");
 	}
 	
 	/**
